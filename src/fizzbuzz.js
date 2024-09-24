d5518578ca18e76681ee2801ca22e031777ae98d
MathiasSaravia <maycolbeach@gmail.com>
2024-09-23 21:33:59 -0300

    first example for TDD

---
+++ src/fizzbuzz.js
@@ -0,0 +1,13 @@
+export const fizzbuzz = (number) => {
+    if (typeof number !== "number") throw new Error("parametre provided must be a number")
+    if (Number.isNaN(number)) throw new Error("parametre provided must be a number")
+
+    const multiplies = { 3 : "fizz" , 5 : "buzz" }
+    let output = ""
+
+    Object.entries(multiplies).forEach(([multiplies , word]) => {
+        if (number % multiplies === 0) output += word
+    })
+    
+    return output === "" ? number : output      
+    }
---
+++ test/fizzbuzz.test.js
@@ -0,0 +1,53 @@
+import {describe , expect, it} from "vitest";
+import {fizzbuzz} from "../src/fizzbuzz.js"
+
+
+describe ("fizzbuzz", ()=> {
+
+/*     it( "should be a function", () => {
+        expect( typeof fizzbuzz ).toBe( "function" )
+    }) */
+
+    it ("should throw if not number is provided as parameter", () => {
+        expect (() => fizzbuzz()).toThrow()
+    })
+
+    it ("should throw a specific error mesage if not number is provided as parameter", () => {
+        expect (() => fizzbuzz()).toThrow("parametre provided must be a number")
+    })
+
+    it ("should throw a specific error mesage if not number is provided as parameter", () => {
+        expect (() => fizzbuzz(NaN)).toThrow("parametre provided must be a number")
+    })
+
+    it ("should return 1 if number provided is 1" , () =>{
+        expect (fizzbuzz(1)).toBe(1)
+    }) 
+
+    it ("should return 2 if number provided is 2" , () =>{
+        expect (fizzbuzz(2)).toBe(2)
+    }) 
+
+    it ("should return fizz if number provided is 3" , () =>{
+        expect (fizzbuzz(3)).toBe("fizz")
+    })
+
+    it ("should return fizz if number provided is multiple of 3 " ,() => {
+        expect (fizzbuzz(9)).toBe("fizz")
+        expect (fizzbuzz(6)).toBe("fizz")
+        expect (fizzbuzz(12)).toBe("fizz")
+    })
+
+    it ("should return buzz if number provided is 5" , () =>{
+        expect (fizzbuzz(5)).toBe("buzz")
+    })
+
+    it ("should return buzz if number provided is multiple of 5" , () =>{
+        expect (fizzbuzz(20)).toBe("buzz")
+        expect (fizzbuzz(10)).toBe("buzz")
+    })
+
+    it ("should return fizzbuzz if number provided is multiple of 15" , () => {
+        expect (fizzbuzz(15)).toBe("fizzbuzz")
+    })
+})
